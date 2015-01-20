6a1705515b75c3f56f2329397c1a1e44ccbb2e7d
fydot <ifidot@gmail.com>
2015-01-20 13:18:28 +0800

    add file.c

--- file.c
+++ file.c
@@ -1,10 +1,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <errno.h>
 
 int main()
 {
 	int fd = open("~/main.c", O_RDONLY);
+    perror("open");
 	printf("%d\n", fd);
 	return 0;
 }
