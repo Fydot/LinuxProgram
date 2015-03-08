058938f2da5a4fed5444035deba4b4de681142bf
fydot <ifidot@gmail.com>
2015-03-08 10:24:00 +0800

    add stream buffer function

---
+++ ch3_buff_fseek_file.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char* names = "hhhhhhhh";
+    FILE *in, *out;
+    int i;
+
+    out = fopen("data", "w");
+    if (!out) {
+        perror("fopen");
+        return 1;
+    }
+    for(i = 0; names[i]; i++) {
+        if(fputc(names[i], out) == EOF) {
+            perror("fputc");
+            return 1;
+        }
+        long pos = ftell(out);
+        printf("pos: %ld\n", pos);
+
+        if (fseek(out, 2, SEEK_CUR) == -1) {
+            perror("fseek");
+            return 1;
+        }
+    }
+}
