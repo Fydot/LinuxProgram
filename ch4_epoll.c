5c3aa1726cfc090d862d9d1a1c23a55cd9018b10
fydot <ifidot@gmail.com>
2015-03-09 21:12:46 +0800

    add epoll io

---
+++ ch4_epoll.c
@@ -0,0 +1,38 @@
+#include <sys/epoll.h>
+
+#define MAX_EVENTS 64
+
+
+int main()
+{
+    int fd, epfd, ret, nr_events, i;
+    struct epoll_event event, *events;
+
+    // 创建 epoll fd
+    if ((epfd = epoll_create1(0))== -1) {
+        perror("epoll_create1");
+        return 1;
+    }
+
+    // 注册标准输入 fd=0
+    event.data.fd = 0;
+    event.events = EPOLLIN | EPOLLOUT;
+    if ((ret = epoll_ctl(epfd, EPOLL_CTL_ADD, 0, event)) == -1) {
+        perror("epoll_ctl");
+        return 1;
+    }
+
+    // 等待 epoll 事件
+    events = malloc(sizeof(struct epoll_event) * MAX_EVENTS);
+    if (!events) {
+        perror("malloc");
+        return 1;
+    }
+
+    nr_events = epoll_wait(epfd, events, MAX_EVENTS, -1);
+    for (i = 0; i < nr_events; i++) {
+        printf("event=%ld on fd=%d\n", events[i].events, events[i].data.fd);
+    }
+    
+    return 0;
+}
